bfae49d69565eb782aa65de9cc74806696ec497c
Chirag-04 <binarycoderz04@gmail.com>
2024-11-04 12:08:15 +0530

    Completed_Hospital_management_system_backend

--- Day6/root.js
+++ Day6/root.js
@@ -1,7 +1,16 @@
 const express = require('express');
 const app = express();
 const port =  3000;
-
+app.use(express.json()); // middleware
+function anyBadKidney(){
+    let isPresent = false;
+    for(let i=0 ; i<users[0].kidneys.length ; i++){
+        if(!users[0].kidneys[i].healthy){
+            isPresent = true;
+        }
+    }
+    return isPresent;
+}
 const users = [
     {
         name: 'john',
@@ -52,18 +61,60 @@ app.get("/" , function(req , res){
    })
 });
 
-// app.post("/" , function(req , res)){
-    
-// }
+// // let say i want to add a kidney 
+app.post("/" , function(req , res){
+    // most common method to take input in post method is body 
+    const isHealthy =  req.body.isHealthy;
+    users[0].kidneys.push({healthy : isHealthy})
+    res.json({
+        msg : "New Kidney added"
+    })
+})
 
-// app.put("/" , function(req , res)){
-    
-// }
+app.put('/' , function(req , res){
+    let isPresentBadKidney =  anyBadKidney();
+    if(!isPresentBadKidney){
+        res.status(411).json({
+            msg : "no unhealthy kidneys"
+        })
+    }
+    for(let i=0 ; i<users[0].kidneys.length ; i++){
+        users[0].kidneys[i].healthy = true;
+    }
 
-// app.delete("/" , function(req , res)){
-    
-// }
+    res.json({
+        msg : "All kidenys updated"
+    })
+})
 
+
+// to delete all the unhealthy kidenys
+
+// couple of ways to achieve this 
+
+// task is to check whether a user has a bad kidney or not 
+
+app.delete('/', function(req , res){
+    let isPresentAnybadKidney =  anyBadKidney();
+    console.log("any bad kidney" ,isPresentAnybadKidney);
+    if(!isPresentAnybadKidney){
+        res.status(411).json({
+            msg : "You dont have any bad kidney"
+        })
+    }
+    const newKidneys = [];
+    for(let i=0; i<users[0].kidneys.length ; i++){
+        if(users[0].kidneys[i].healthy){
+            newKidneys.push({
+                healthy : true
+            })
+        }
+    }
+    users[0].kidneys = newKidneys;
+    res.json({
+        msg : "unhealthy kidneys removed"
+    })
+})
 
 app.listen(port , function(){
     console.log("Server running!!")
